337add32b09a04a8f081ad1f2e3df81e914bc717
Kelve Aragão <kelvearagao@gmail.com>
2019-12-15 18:51:26 -0300

    Creating User Mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -117,6 +117,30 @@ const RootQuery = new GraphQLObjectType({
     }
 })
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        createUser: {
+            type: UserType,
+            args: {
+                name: { type: GraphQLString },
+                age: { type: GraphQLInt },
+                profession: { type: GraphQLString}
+            },
+            resolve(parent, args) {
+                const user = {
+                    name: args.name,
+                    age: args.age,
+                    profession: args.profession
+                }
+
+                return user
+            }
+        }
+    }
+})
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 })
